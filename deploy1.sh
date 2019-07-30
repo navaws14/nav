bcd24c9227dd32664950b97efc713e27fda5b4a0
navaws14 <navaws14@gmail.com>
2019-07-30 22:03:09 +0530

    updated

--- deploy1.sh
+++ deploy1.sh
@@ -1,1 +1,11 @@
 echo "software developement process flow for git"
+
+
+<< processe 
+
+we need to create empty respository locally with using of git init
+then that file to move the stage or index area and commit to local respistory.
+processe
+
+
+
